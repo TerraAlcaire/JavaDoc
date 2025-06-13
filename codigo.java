b3572ee3d5b4a38767a9116a743bd57b903e948b
terra <terraalcaire@gmail.com>
2025-06-13 12:22:30 +0200

    Codigo base sin documentación

---
+++ codigo.java
@@ -0,0 +1,47 @@
+public class Libro {
+
+    private String titulo;
+    private String autor;
+    private int anioPublicacion;
+
+    public Libro(String titulo, String autor, int anioPublicacion) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public int calcularAntiguedad() {
+        int anioActual = java.time.Year.now().getValue();
+        return anioActual - this.anioPublicacion;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Año de publicación: " + anioPublicacion);
+    }
+}
